f323103dc9ea9e0435d870629ca8091d285346a5
tlot02 <64542338+tlot02@users.noreply.github.com>
2021-01-19 21:54:12 -0500

    adds overlay navigation. will need to update css for nav button

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+function openNav() {
+  document.getElementById("overlayNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("overlayNav").style.height = "0%";
+}
